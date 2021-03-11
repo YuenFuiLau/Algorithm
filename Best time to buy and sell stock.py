4858fdca0aadec3d258fd40106a2ee7187dd6d82
Amir Lau <67386096+Amir-D-Shadow@users.noreply.github.com>
2021-03-12 01:06:55 +0800

    Dynamic Programming

---
+++ Best time to buy and sell stock.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+
+def inf_MaxProfit(prices):
+
+    dp_i_0 = 0
+    dp_i_1 = -max(prices)-5
+
+    for stock in prices:
+
+        prev_dp_0 = dp_i_0
+
+        dp_i_0 = max(dp_i_0,dp_i_1 + stock)
+        dp_i_1 = max(dp_i_1,prev_dp_0 - stock)
+        
+
+    return dp_i_0
+
+
+
+
+
+def maxProfit(prices,k=2):
+
+
+    dp = np.zeros((len(prices),k+1,2))
+
+    if 2*k > len(prices):
+
+        return inf_MaxProfit(prices)
+
+    
+
+    for j in range(1,k+1):
+        
+
+        for i in range(len(prices)):
+
+            if i - 1 < 0:
+
+                dp[i][j][0] = 0
+                dp[i][j][1] = -prices[i]
+
+                continue
+
+            dp[i][j][0] = max(dp[i-1][j][0],dp[i-1][j][1]+prices[i])
+
+            dp[i][j][1] = max(dp[i-1][j][1],dp[i-1][j-1][0]-prices[i])
+
+
+    return dp[len(prices)-1][2][0]
+
+
+a =  [0,5,0,3]
+
+a =  [3,3,5,0,0,3,1,4]
+
+b = [7,1,5,3,6,4]
+
+test = inf_MaxProfit(b)
+
+ans = maxProfit(a)
+
+dp = np.zeros((2,1,2))
+
+a =  [3,3,5,0,0,3,1,4]
+
+
